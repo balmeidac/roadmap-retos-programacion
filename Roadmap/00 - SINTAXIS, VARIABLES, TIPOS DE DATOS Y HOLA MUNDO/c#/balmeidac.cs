453a67f7212f30a481e8c3535f919420ff0c71ea
Byron Omar Almeida Coello <byronalmeidaec@gmail.com>
2024-10-12 15:47:37 -0500

    Primer comentario de ejercicio 00 creado por balmeidac

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/balmeidac.cs
@@ -0,0 +1,69 @@
+// EJERCICIO 00 SINTAXUS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO:
+//
+//  * Crea un comentario en el código y coloca la URL del sitio web oficial del
+//    lenguaje de programación que has seleccionado.
+//
+//      Sitio Web oficial de C Sharp >> https://learn.microsoft.com/es-es/dotnet/csharp/
+//
+//  * Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+//
+//      En C# podemos comentar de 3 formas:
+//
+//      1.-  Comentarios de una sola linea mediante un slash doble o barra inclinada doble: //
+//         
+//              Por Ejemplo: // Este es un comentario de una sola línea.
+//         
+//      2.-  Comentarios de varias líneas, mediante un slash y asterisco de esta forma: /* */
+//         
+//              Por Ejemplo: /* De esta forma podemos comentar, varias líneas
+//                           agregando un slash y un asterisco al inicio
+//                           y al final del texto a comentar */
+//
+//      3.-  Comentarios de documentación, usa /// antes de una clase, método o propiedad
+//           para generar comentarios de documentación en XML. Es tipo de comentario se lo utiliza
+//           para proyectos grandes.
+//
+//               Por Ejemplo:
+///               <summary>
+///               Este método suma dos números.
+///               </summary>
+///               <param name="a">Primer número</param>
+///               <param name="b">Segundo número</param>
+///               <returns>La suma de ambos números</returns>
+///                
+//                public int Sumar(int a, int b)
+//                {
+//                  return a + b;
+//                }
+//
+// * - Crea una variable (y una constante si el lenguaje lo soporta).
+//
+//      Variable de tipo cadena de texto.
+//      string reto = "Sintaxis, variables, tipos de datos y Hola mundo!";
+//
+//      Constante tipo decimal, la letra m o M al final del valor numérico lo define como decimal.
+//      const decimal PI = 3.14159m
+//
+// * - Crea variables representando todos los tipos de datos primitivos 
+//     del lenguaje (cadenas de texto, enteros, booleanos...).
+//      
+//      Variable de tipo entero
+//      int edad = 25;
+//
+//      Variable de tipo cadena de texto
+//      string nombre = "David";
+//
+//      Variable de tipo decimal
+//      decimal precio = 19.99m;
+//
+//      Variable de tipo booleano
+//      bool esCientifico = true;
+//      boll esCientifico = false;
+//
+//      Variable de tipo double
+//      double velocidadLuz = 299792458.0
+//
+//  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+//  
+//      Console.WriteLine("¡Hola, Lenguaje de Programación C Sharp!");
+//
